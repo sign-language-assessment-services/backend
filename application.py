2e8f41b56ad1e5765ecc31a082703a7af467eca5
Danny Rehl <15615045+colidyre@users.noreply.github.com>
2020-06-26 20:38:26 +0200

    Added missing module docstring

--- application.py
+++ application.py
@@ -1,3 +1,14 @@
+"""Get Fastapi app object and run it only if directly invoked
+
+This file provides the app object from `app.main` directly. It can be
+used to start a (u)wsgi server which handles the app object or using
+gunicorn or similar webserver.
+
+The file can also be started as a script. Then `uvicorn` will start the
+app being a local web server. This is very helpful to debug the app as
+described here: https://fastapi.tiangolo.com/tutorial/debugging/ .
+"""
+
 from app.main import app
 
 if __name__ == "__main__":
