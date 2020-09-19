5087955b8ccfafdb00924d44f30b50787a6addc5
Dustin Heckmann <kontakt@dustin-heckmann.de>
2020-09-19 16:34:15 +0200

    Fixed linting issues

--- app/core/test/test_assessment.py
+++ app/core/test/test_assessment.py
@@ -5,37 +5,37 @@ from app.core.models.choice import Choice
 from app.core.models.multiple_choice import MultipleChoice
 
 
-@pytest.fixture()
+@pytest.fixture(name="mocked_assessment")
 def assessment():
     return Assessment(
         name="foo",
         items=(
             MultipleChoice(
-                description="bar",
+                description="description 1",
                 choices=(
-                    Choice(label="foo", is_correct=False),
-                    Choice(label="foo", is_correct=True),
+                    Choice(label="choice 1-A", is_correct=False),
+                    Choice(label="choice 1-B", is_correct=True),
                 )
             ),
             MultipleChoice(
-                description="bar",
+                description="description 2",
                 choices=(
-                    Choice(label="foo", is_correct=True),
-                    Choice(label="foo", is_correct=False),
-                    Choice(label="foo", is_correct=True),
+                    Choice(label="choice 2-A", is_correct=True),
+                    Choice(label="choice 2-B", is_correct=False),
+                    Choice(label="choice 2-C", is_correct=True),
                 )
             )
         )
     )
 
 
-def test_perfect_score(assessment):
-    assert assessment.score({0: [1], 1: [0, 2]}) == {"score": 2}
+def test_perfect_score(mocked_assessment):
+    assert mocked_assessment.score({0: [1], 1: [0, 2]}) == {"score": 2}
 
 
-def test_fifty_percent(assessment):
-    assert assessment.score({0: [1], 1: [0, 1]}) == {"score": 1}
+def test_fifty_percent(mocked_assessment):
+    assert mocked_assessment.score({0: [1], 1: [0, 1]}) == {"score": 1}
 
 
-def test_total_failure(assessment):
-    assert assessment.score({0: [0], 1: [1]}) == {"score": 0}
+def test_total_failure(mocked_assessment):
+    assert mocked_assessment.score({0: [0], 1: [1]}) == {"score": 0}
