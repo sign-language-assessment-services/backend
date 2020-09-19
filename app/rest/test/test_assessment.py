aac4d441e00e6739ab26863656017254d4747f24
Danny Rehl <danny.rehl@altechnologies.de>
2020-09-19 18:00:21 +0200

    Tests for POST request for scoring assessment

--- app/rest/test/test_assessment.py
+++ app/rest/test/test_assessment.py
@@ -1,4 +1,4 @@
-import pytest
+from unittest.mock import patch
 
 from .base import get_test_client
 
@@ -55,13 +55,19 @@ def test_get_assessment():
     }
 
 
-@pytest.mark.skip(reason="Only temporary")
-def test_post_assessment():
-    data = {
+@patch("app.core.interactors.assessments.score_assessment")
+def test_post_assessment(score_asssessment_mock):
+    assessment_id = 1
+    score_asssessment_mock.return_value = {"score": 2}
+    submission = {
         0: [1],
         1: [0, 2]
     }
 
-    response = CLIENT.post("/assessments/1/submissions/", data=data)
+    response = CLIENT.post(
+        f"/assessments/{assessment_id}/submissions/", data=submission
+    )
+
+    score_asssessment_mock.assert_called_once_with(assessment_id, submission)
     assert response.status_code == 200
     assert response.json() == {"score": 2}
