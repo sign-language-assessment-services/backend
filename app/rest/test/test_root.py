d4449673922ec389d31c967c930146022b57c02b
Danny Rehl <15615045+colidyre@users.noreply.github.com>
2023-02-12 14:48:53 +0100

    Fix root endpoint test

--- app/rest/test/test_root.py
+++ app/rest/test/test_root.py
@@ -6,4 +6,9 @@ CLIENT = get_test_client()
 def test_get_root():
     response = CLIENT.get("/")
     assert response.status_code == 200
-    assert response.json() == {"msg": "Hello World!"}
+    assert response.json() == {"status": "ok"}
+
+def test_health():
+    response = CLIENT.get("/health")
+    assert response.status_code == 200
+    assert response.json() == {"status": "ok"}
