efc590c97197687f2082c5e0f4a30d7ef8ef1728
Danny Rehl <15615045+colidyre@users.noreply.github.com>
2023-04-10 22:30:41 +0200

    Fix mypy issues

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -1,19 +1,21 @@
+from typing import Any
+
 import pytest
 
 from app.utils import strtobool
 
 
 @pytest.mark.parametrize("val", ["y", "yes", "t", "true", "on", "1"])
-def test_strtobool_returns_true(val):
+def test_strtobool_returns_true(val: str) -> None:
     assert strtobool(val) is True
 
 
 @pytest.mark.parametrize("val", ["n", "no", "f", "false", "off", "0"])
-def test_strtobool_returns_false(val):
+def test_strtobool_returns_false(val: str) -> None:
     assert strtobool(val) is False
 
 
 @pytest.mark.parametrize("val", ["foo", None, [1, 2, 3], len, ...])
-def test_strtobool_returns_default(val):
+def test_strtobool_returns_default(val: Any) -> None:
     assert strtobool(val) is False
     assert strtobool(val, default=True) is True
