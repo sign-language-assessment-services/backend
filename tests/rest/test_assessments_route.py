2d61cac8fae800c2a8eb9b68139791d4a2f94c90
Danny Rehl <15615045+colidyre@users.noreply.github.com>
2023-05-06 11:32:41 +0200

    Fix test

--- tests/rest/test_assessments_route.py
+++ tests/rest/test_assessments_route.py
@@ -8,43 +8,48 @@ def test_get_assessment(get_presigned_url_mock: Mock, test_client: TestClient) -
     get_presigned_url_mock.return_value = "http://data.localhost/slportal/witch.mp4"
 
     response = test_client.get("/assessments/1")
+    json_response = response.json()
+
     assert response.status_code == 200
-    assert response.json()["items"][0] == {
-        'question': {
-            'location': {
-                'bucket': 'slportal',
-                'key': 'slas_sgs_gv/exercises/1/01_Frage.mp4'
+    assert json_response["name"] == "SLAS DSGS GV"
+    assert len(json_response["items"]) == 7
+    assert json_response["items"][0] == {
+        "question": {
+            "location": {
+                "bucket": "slportal",
+                "key": "slas_sgs_gv/exercises/1/01_Frage.mp4"
             },
-            'url': '',
-            'type': 'video'
+            "url": "http://data.localhost/slportal/witch.mp4",
+            "type": "video"
         },
-        'choices': [
+        "choices": [
             {
-                'location': {
-                    'bucket': 'slportal',
-                    'key': 'slas_sgs_gv/exercises/1/01a_Antwort.mp4'
+                "location": {
+                    "bucket": "slportal",
+                    "key": "slas_sgs_gv/exercises/1/01a_Antwort.mp4"
                 },
-                'is_correct': False,
-                'url': 'http://data.localhost/slportal/witch.mp4',
-                'type': 'video'
+                "is_correct": False,
+                "url": "http://data.localhost/slportal/witch.mp4",
+                "type": "video"
             },
             {
-                'location': {
-                    'bucket': 'slportal',
-                    'key': 'slas_sgs_gv/exercises/1/01b_Antwort.mp4'
+                "location": {
+                    "bucket": "slportal",
+                    "key": "slas_sgs_gv/exercises/1/01b_Antwort.mp4"
                 },
-                'is_correct': True,
-                'url': 'http://data.localhost/slportal/witch.mp4',
-                'type': 'video'
+                "is_correct": True,
+                "url": "http://data.localhost/slportal/witch.mp4",
+                "type": "video"
             },
             {
-                'location': {
-                    'bucket': 'slportal',
-                    'key': 'slas_sgs_gv/exercises/1/01c_Antwort.mp4'
+                "location": {
+                    "bucket": "slportal",
+                    "key": "slas_sgs_gv/exercises/1/01c_Antwort.mp4"
                 },
-                'is_correct': False,
-                'url': 'http://data.localhost/slportal/witch.mp4',
-                'type': 'video'}
+                "is_correct": False,
+                "url": "http://data.localhost/slportal/witch.mp4",
+                "type": "video"
+            }
         ]
     }
 
