1cc8bc4de32d77708c0e22e6bda727b5640a351f
Danny Rehl <15615045+colidyre@users.noreply.github.com>
2024-09-30 11:38:53 +0200

    Fix position (starting from 0)

--- initial_queries.sql
+++ initial_queries.sql
@@ -6,8 +6,8 @@ INSERT INTO multimedia_files VALUES ('9feab952-0730-433a-9c36-78080f29b3d4', now
 INSERT INTO multimedia_files VALUES ('2d8f4f20-4bd2-42ec-add4-30adbf8f9132', now(), 'slportal', 'SLAS DSGS GV/03/01b_Antwort.mp4', 'VIDEO');
 INSERT INTO multimedia_files VALUES ('bee674ed-4440-488d-b22f-1a00ac77952c', now(), 'slportal', 'SLAS DSGS GV/03/01c_Antwort_richtig.mp4', 'VIDEO');
 
-INSERT INTO primers VALUES ('ace257e6-85d9-4017-9e7c-645a3812c8fc', now(), 1, 'c4d129f9-41da-4a79-9f24-b9d79f1e86aa', '28669db7-9c97-422c-a7b7-afd3b5a964a5');
-INSERT INTO exercises VALUES ('d24b2d12-dcac-4747-8d94-84b7002df647', now(), 2, 'c4d129f9-41da-4a79-9f24-b9d79f1e86aa', '9cc09540-dd71-456b-a311-8ae187849d72');
+INSERT INTO primers VALUES ('ace257e6-85d9-4017-9e7c-645a3812c8fc', now(), 0, 'c4d129f9-41da-4a79-9f24-b9d79f1e86aa', '28669db7-9c97-422c-a7b7-afd3b5a964a5');
+INSERT INTO exercises VALUES ('d24b2d12-dcac-4747-8d94-84b7002df647', now(), 1, 'c4d129f9-41da-4a79-9f24-b9d79f1e86aa', '9cc09540-dd71-456b-a311-8ae187849d72');
 
 INSERT INTO choices VALUES ('e57175a9-ff4e-4368-bc14-8156c252274d', now(), false, 'd24b2d12-dcac-4747-8d94-84b7002df647', '9feab952-0730-433a-9c36-78080f29b3d4');
 INSERT INTO choices VALUES ('216c487a-a9e7-481b-8475-bb9f1ec3418b', now(), false, 'd24b2d12-dcac-4747-8d94-84b7002df647', '2d8f4f20-4bd2-42ec-add4-30adbf8f9132');
