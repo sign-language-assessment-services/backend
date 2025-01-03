a3b03beb1d5dc3f2057990998c11cf3b8544cb3b
Danny Rehl <15615045+colidyre@users.noreply.github.com>
2025-01-03 20:23:31 +0100

    Fix assessments tests

--- tests/rest/test_assessments_route.py
+++ tests/rest/test_assessments_route.py
@@ -15,9 +15,11 @@ def test_get_assessment(test_client: TestClient) -> None:
     assert response.json()["id"] == assessment_id
     assert response.json()["name"] == "Test Assessment 1"
     assert len(response.json()["tasks"]) == 2
-    for _id in response.json()["tasks"]:
-        assert isinstance(_id, str)
-        assert UUID(_id)
+    for task in response.json()["tasks"]:
+        assert isinstance(task.get("id"), str)
+        assert UUID(task.get("id"))
+    assert response.json()["tasks"][0]["task_type"] == "primer"
+    assert response.json()["tasks"][1]["task_type"] == "exercise"
 
 
 def test_get_assessment_not_found(test_client_no_assessment: TestClient) -> None:
@@ -36,8 +38,13 @@ def test_list_assessments(test_client: TestClient) -> None:
     assert response.json()[0]["id"] == "a0000000-0000-0000-0000-000000000001"
     assert response.json()[0]["name"] == "Test Assessment 1"
     assert response.json()[1]["name"] == "Test Assessment 2"
-    assert len(response.json()[0]["tasks"]) == 2
-    assert len(response.json()[1]["tasks"]) == 2
+
+
+def test_list_assessments_does_not_show_tasks(test_client: TestClient) -> None:
+    response = test_client.get("/assessments/")
+
+    assert response.status_code == HTTP_200_OK
+    assert not any("tasks" in assessment for assessment in response.json())
 
 
 @pytest.mark.parametrize("endpoint", ["/assessments/", f"/assessments/{uuid4()}"])
