2b7aa6ad0146e86d642709939e3967c9a9cd01ad
Danny Rehl <15615045+colidyre@users.noreply.github.com>
2025-01-11 19:55:08 +0100

    Fix endpoints post status tests

--- tests/rest/routes/test_endpoints_status_codes.py
+++ tests/rest/routes/test_endpoints_status_codes.py
@@ -1,3 +1,5 @@
+from uuid import uuid4
+
 import pytest
 from fastapi import status
 from starlette.testclient import TestClient
@@ -8,7 +10,7 @@ from tests.data.models.multimedia_files import multimedia_file_choice_1
 from tests.data.models.primers import primer_1
 from tests.data.models.submissions import submission_1
 
-GET_BY_ID_ENDPOINTS = [
+GET_ENDPOINTS = [
     f"/assessments/{str(assessment_1.id)}",
     f"/exercises/{str(exercise_1.id)}",
     f"/object-storage/{str(multimedia_file_choice_1.id)}",
@@ -16,9 +18,6 @@ GET_BY_ID_ENDPOINTS = [
     f"/submissions/{str(submission_1.id)}"
 ]
 
-POST_ENDPOINTS = [
-    f"/assessments/{str(assessment_1.id)}/exercises/{str(exercise_1.id)}/submissions/"
-]
 
 LIST_ENDPOINTS = [
     "/assessments/",
@@ -29,22 +28,40 @@ LIST_ENDPOINTS = [
 ]
 
 
-@pytest.mark.parametrize("endpoint", GET_BY_ID_ENDPOINTS + POST_ENDPOINTS + LIST_ENDPOINTS)
+@pytest.mark.parametrize("endpoint", GET_ENDPOINTS + LIST_ENDPOINTS)
 def test_get_200(endpoint: str, test_client: TestClient) -> None:
     response = test_client.get(endpoint)
 
     assert response.status_code == status.HTTP_200_OK
 
 
-@pytest.mark.parametrize("endpoint", GET_BY_ID_ENDPOINTS + POST_ENDPOINTS + LIST_ENDPOINTS)
+@pytest.mark.parametrize("endpoint", GET_ENDPOINTS + LIST_ENDPOINTS)
 def test_get_403(endpoint: str, test_client_no_roles: TestClient) -> None:
     response = test_client_no_roles.get(endpoint)
 
     assert response.status_code == status.HTTP_403_FORBIDDEN
 
 
-@pytest.mark.parametrize("endpoint", GET_BY_ID_ENDPOINTS)
+@pytest.mark.parametrize("endpoint", GET_ENDPOINTS)
 def test_get_not_found(endpoint: str, test_client_not_found: TestClient) -> None:
     response = test_client_not_found.get(endpoint)
 
     assert response.status_code == status.HTTP_404_NOT_FOUND
+
+
+def test_post_submission_200(test_client: TestClient) -> None:
+    endpoint = f"/assessments/{str(assessment_1.id)}/exercises/{str(exercise_1.id)}/submissions/"
+    data = {"choices": [str(uuid4())]}
+
+    response = test_client.post(endpoint, json=data)
+
+    assert response.status_code == status.HTTP_200_OK
+
+
+def test_post_submission_403(test_client_no_roles: TestClient) -> None:
+    endpoint = f"/assessments/{str(assessment_1.id)}/exercises/{str(exercise_1.id)}/submissions/"
+    data = {"choices": [str(uuid4())]}
+
+    response = test_client_no_roles.post(endpoint, json=data)
+
+    assert response.status_code == status.HTTP_403_FORBIDDEN
