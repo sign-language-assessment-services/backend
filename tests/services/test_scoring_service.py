00e41e7aa53ebe8092874371b2a36b06dc324176
Danny Rehl <15615045+colidyre@users.noreply.github.com>
2025-04-21 12:28:27 +0200

    Fix scoring tests by not using references, which will make problems if running tests together

--- tests/services/test_scoring_service.py
+++ tests/services/test_scoring_service.py
@@ -1,3 +1,4 @@
+from copy import deepcopy
 from uuid import uuid4
 
 from data.models.exercise_submissions import exercise_submission_1, exercise_submission_2
@@ -8,46 +9,53 @@ from app.services.scoring_service import ScoringService
 
 def test_correct_single_multiple_choice_answer_scores_exercise_points() -> None:
     scoring_service = ScoringService()
-    exercise_score_before = exercise_submission_1.score
+    exercise_submission_under_test = deepcopy(exercise_submission_1)
+    submission_score_before = exercise_submission_under_test.score
 
-    scoring_service.score(exercise_submission_1, exercise_1)
-    exercise_score_after = exercise_submission_1.score
+    scoring_service.score(exercise_submission_under_test, exercise_1)
+    submission_score_after = exercise_submission_under_test.score
 
-    assert exercise_score_before is None
-    assert exercise_score_after == exercise_1.points
+    assert submission_score_before is None
+    assert submission_score_after == exercise_1.points
 
 
 def test_correct_multiple_multiple_choice_answers_scores_exercise_points() -> None:
     scoring_service = ScoringService()
-    exercise_score_before = exercise_submission_1.score
-    exercise_1.question_type.content.choices[1].is_correct = True
-    exercise_submission_1.answer.choices += [exercise_1.question_type.content.choices[1].id]
+    exercise_under_test = deepcopy(exercise_1)
+    exercise_submission_under_test = deepcopy(exercise_submission_1)
+    exercise_under_test.question_type.content.choices[1].is_correct = True
+    exercise_submission_under_test.answer.choices += [
+        exercise_under_test.question_type.content.choices[1].id
+    ]
+    submission_score_before = exercise_submission_under_test.score
 
-    scoring_service.score(exercise_submission_1, exercise_1)
-    exercise_score_after = exercise_submission_1.score
+    scoring_service.score(exercise_submission_under_test, exercise_under_test)
+    submission_score_after = exercise_submission_under_test.score
 
-    assert exercise_score_before is None
-    assert exercise_score_after == exercise_1.points
+    assert submission_score_before is None
+    assert submission_score_after == exercise_under_test.points
 
 
 def test_incorrect_single_multiple_choice_answer_scores_0() -> None:
     scoring_service = ScoringService()
-    exercise_score_before = exercise_submission_2.score
+    exercise_submission_under_test = deepcopy(exercise_submission_2)
+    submission_score_before = exercise_submission_under_test.score
 
-    scoring_service.score(exercise_submission_2, exercise_1)
-    exercise_score_after = exercise_submission_2.score
+    scoring_service.score(exercise_submission_under_test, exercise_1)
+    submission_score_after = exercise_submission_under_test.score
 
-    assert exercise_score_before is None
-    assert exercise_score_after == 0
+    assert submission_score_before is None
+    assert submission_score_after == 0
 
 
 def test_partially_correct_multiple_choice_answer_scores_0() -> None:
     scoring_service = ScoringService()
-    exercise_score_before = exercise_submission_1.score
-    exercise_submission_1.answer.choices += [uuid4()]
+    exercise_submission_under_test = deepcopy(exercise_submission_1)
+    exercise_submission_under_test.answer.choices += [uuid4()]
+    submission_score_before = exercise_submission_under_test.score
 
-    scoring_service.score(exercise_submission_1, exercise_1)
-    exercise_score_after = exercise_submission_1.score
+    scoring_service.score(exercise_submission_under_test, exercise_1)
+    submission_score_after = exercise_submission_under_test.score
 
-    assert exercise_score_before is None
-    assert exercise_score_after == 0
+    assert submission_score_before is None
+    assert submission_score_after == 0
