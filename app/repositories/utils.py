12ca7f71f9f022084620d96b1f16a558da9c6b20
Danny Rehl <15615045+colidyre@users.noreply.github.com>
2025-10-03 11:00:12 +0200

    Improve generic database queries

--- app/repositories/utils.py
+++ app/repositories/utils.py
@@ -1,77 +1,107 @@
 import logging
 from collections.abc import Mapping
-from datetime import datetime, timezone
-from typing import Any, Iterable, Type, TypeAlias, TypeVar
+from typing import Any, Iterator, Type, TypeAlias, TypeVar
 from uuid import UUID
 
-from sqlalchemy import ColumnCollection, and_, select, update
+from sqlalchemy import delete, func, inspect, select, update
 from sqlalchemy.dialects.postgresql import insert
-from sqlalchemy.orm import DeclarativeBase, InstrumentedAttribute, Session
+from sqlalchemy.orm import InstrumentedAttribute, Session
 from sqlalchemy.sql.schema import ColumnCollectionConstraint, Index
 
 from app.database.exceptions import EntryNotFoundError
+from app.database.tables.base import DbBase
 
 logger = logging.getLogger(__name__)
 
-T = TypeVar("T", bound=DeclarativeBase)
+T = TypeVar("T", bound=DbBase)
 OnUpdateConstraint: TypeAlias = str | ColumnCollectionConstraint | Index | None
-OnUpdateFields: TypeAlias = Mapping[Any, Any] | ColumnCollection | None
 
 
-def add_entry(session: Session, db: T) -> None:
+def add_entry(session: Session, db: T, commit: bool = True) -> None:
+    logger.info(
+        "Using generic database request to add %(_id)s from %(_class)s with session id %(session_id)s.",
+        {"_id": db.id, "_class": db.__class__.__name__, "session_id": id(session)}
+    )
     session.add(db)
-    session.flush()
+    if commit:
+        session.commit()
 
 
 def get_by_id(session: Session, _class: Type[T], _id: UUID) -> T | None:
-    return session.execute(select(_class).filter_by(id=_id)).unique().scalar_one_or_none()
+    logger.info(
+        "Using generic database request to receive %(_id)s from %(_class)s with session id %(session_id)s.",
+        {"_id": _id, "_class": _class.__name__, "session_id": id(session)}
+    )
+    return session.get(_class, _id)
 
 
-def get_all(session: Session, _class: Type[T], filter_by: dict[InstrumentedAttribute, Any] = None) -> Iterable[T]:
+def get_all(session: Session, _class: Type[T], filters: dict[InstrumentedAttribute, Any] = None) -> Iterator[T]:
     query = select(_class)
-    if filter_by:
-        logger.debug(
-            "Filter used in querying %(_class)s: %(filter)r",
-            {"_class": _class.__name__, "filter": filter_by}
+    if filters:
+        logger.info(
+            "Querying %(_class)s with filters: %(filters)r",
+            {"_class": _class.__name__, "filters": filters}
         )
-        conditions = [key == value for key, value in filter_by.items()]
-        query = query.where(and_(*conditions))
-    return session.execute(query).unique().scalars().all()
+        for column, value in filters.items():
+            query = query.where(column == value)
+    logger.info(
+        "Using generic database request to receive all from %(_class)s with session id %(session_id)s.",
+        {"_class": _class.__name__, "session_id": id(session)}
+    )
+    return session.execute(query).scalars()
 
 
-def update_entry(session: Session, _class: Type[T], _id: UUID, **kwargs) -> None:
+def update_entry(session: Session, _class: Type[T], _id: UUID, commit: bool = True, **kwargs) -> None:
+    logger.info(
+        "Using generic database request to update %(_id)s from %(_class)s with session id %(session_id)s.",
+        {"_id": _id, "_class": _class.__name__, "session_id": id(session)}
+    )
     session.execute(update(_class).where(_class.id == _id).values(**kwargs))
-    session.flush()
+    if commit:
+        session.commit()
 
 
 def upsert_entry(
         session: Session,
         db: T,
         on_constraint: OnUpdateConstraint,
-        fields_to_update: OnUpdateFields
+        fields_to_update: Mapping[Any, Any],
+        commit: bool = True
 ) -> None:
-    db_class: Type[T] = db.__class__
+    db_class: Type[T] = type(db)
+    logger.info(
+        "Using generic database request to upsert %(_id)s from %(_class)s with session id %(session_id)s.",
+        {"_id": db.id, "_class": db.__class__.__name__, "session_id": id(session)}
+    )
     insert_stmt = insert(db_class).values(
-        **{
-            k: v for k, v in db.__dict__.items()
-            if not k.startswith("_")
+        {
+            col.key: getattr(db, col.key)
+            for col in inspect(db_class).columns
         }
     )
     upsert_stmt = insert_stmt.on_conflict_do_update(
         constraint=on_constraint,
-        set_={
+        set_=
+        {
             **fields_to_update,
-            db_class.modified_at: datetime.now(timezone.utc)
+            db_class.modified_at: func.now()
         }
     )
     session.execute(upsert_stmt)
+    if commit:
+        session.commit()
 
 
-def delete_entry(session: Session, _class: Type[T], _id: UUID) -> None:
-    entry = session.get(_class, _id)
-    if entry is None:
+def delete_entry(session: Session, _class: Type[T], _id: UUID, commit: bool = True) -> None:
+    logger.info(
+        "Using generic database request to delete %(_id)s from %(_class)s with session id %(session_id)s.",
+        {"_id": _id, "_class": _class, "session_id": id(session)}
+    )
+    statement = delete(_class).where(_class.id == _id)
+    result = session.execute(statement)
+    if result.rowcount == 0:
         raise EntryNotFoundError(
             f"Table '{_class.__tablename__}' has no entry with id '{_id}'."
         )
-    session.delete(entry)
-    session.flush()
+    if commit:
+        session.commit()
