1137a025726894b85d8c58dacde4d1fd6640e226
Danny Rehl <15615045+colidyre@users.noreply.github.com>
2025-11-09 19:01:38 +0100

    Deduplicate endpoint decorator data

--- app/rest/routers/root.py
+++ app/rest/routers/root.py
@@ -4,28 +4,19 @@ from slowapi.util import get_remote_address
 
 limiter = Limiter(key_func=get_remote_address)
 router = APIRouter(tags=["Health & Monitoring"])
-
-
-@router.get(
-    "/health",
-    response_model=dict[str, str],
-    status_code=status.HTTP_200_OK,
-    description=(
+metadata = {
+    "response_model": dict[str, str],
+    "status_code": status.HTTP_200_OK,
+    "description": (
         "The objective of this endpoint is to return {'status': 'ok'} if the "
         "application is running. It does not distinguish between health and "
         "readiness, i.e. does not check if underlying services are healthy. "
     )
-)
-@router.get(
-    "/",
-    response_model=dict[str, str],
-    status_code=status.HTTP_200_OK,
-    description=(
-        "The objective of this endpoint is to return {'status': 'ok'} if the "
-        "application is running. It does not distinguish between health and "
-        "readiness, i.e. does not check if underlying services are healthy. "
-    )
-)
+}
+
+
+@router.get("/health", **metadata)
+@router.get("/", **metadata)
 @limiter.limit(
     limit_value="30/minute",
     error_message="Too many requests: Only 30 requests per minutes are allowed."
